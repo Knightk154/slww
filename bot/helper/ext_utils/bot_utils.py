21f55beb751494ea86da1524151ee7b06c61bd60
Hfijdronu9 <87329938+Hfijdronu9@users.noreply.github.com>
2021-07-13 10:40:18 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -107,7 +107,7 @@ def get_readable_message():
                     msg += f"\n<b>Downloaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
                 else:
                     msg += f"\n<b>Uploaded:</b> {get_readable_file_size(download.processed_bytes())} of {download.size()}"
-                msg += f"\n<b>Speed ⚡️:</b> {download.speed()}\n<b>ETA:</b> {download.eta()} "
+                msg += f"\n<b>Speed ⚡️:</b> {download.speed()}\n<b>Estimated time of arrival ⏳:</b> {download.eta()} "
                 # if hasattr(download, 'is_torrent'):
                 try:
                     msg += f"\n<b>Seeders🌹:</b> {download.aria_download().num_seeders}" \
